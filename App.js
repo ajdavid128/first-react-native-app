7b054cadf2d3342158d14b4396d77fdbac858185
Aaron David <ajdavid128@gmail.com>
2023-04-04 16:36:56 -0500

    learning about the new JSX elements and attributes

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
 
+  const handlePress = () => {
+    console.log("text pressed")
+  }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
+      <Image source={require('./assets/favicon.png')}/>
+      <Text numberOfLines={1} onPress={handlePress}>This is a demonstration of how text can be truncated using the 'numberOfLines' attribute</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
