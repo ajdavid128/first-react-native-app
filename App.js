dc82caf41823139fb88981b8e652912909c42a1a
Aaron David <ajdavid128@gmail.com>
2023-04-05 11:34:31 -0500

    figured out how to utilize useDeviceOrientation and found that I cannot destructure it as an object into it's respective types unless it is in string type

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Alert, Button, Text,TouchableHighlight, View, Image, SafeAreaView, Platform, Dimensions } from 'react-native';
 // SafeAreaView is an iOS only feature. Androids will need extra buffers 
+import { useWindowDimensions } from 'react-native';
+import {useDeviceOrientation} from '@react-native-community/hooks'
+
+
+
 export default function App() {
 
-  console.log(Dimensions.get("screen"));
+  // console.log(Dimensions.get("screen"));
+// This will tell me the dimensions of the screen my app is working on
+
+  // console.log(useWindowDimensions(window));
+  // This will also display the dimension of the screen my app is working on, but will account for orientation
+
+  // console.log(useDeviceOrientation());
+  // This will log whether my device is in landscape or portrait mode
+
+  const orientation = useDeviceOrientation();
+// console.log(useDeviceOrientation() === 'landscape');
+  // by utilizing useDeviceOrientation in this way I can take the object and dynamically style components to orientate a specific way depending on whether the device is in landscape or portrait mode
+  // I am unable to deconstruct the useDeviceOrientation into it's respective objects as variables and must refer to them as strings
 
   const handlePress = () => {
     console.log("text pressed")
@@ -20,7 +37,7 @@ export default function App() {
       <View style={{
         backgroundColor: "blue",
         width: '100%',
-        height: '30%'
+        height: orientation === 'landscape' ? '100%' : '30%',
         }}>
 
       </View>
@@ -53,7 +70,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    // justifyContent: 'center',
+    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
